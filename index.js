cbbead367513a13eb4f54c626a9d0405dc2e7c15
Ben Greenier <ben@bengreenier.com>
2022-02-07 15:36:07 -0800

    feat(init): Initial version

--- index.js
+++ index.js
@@ -1,18 +1,110 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const globber = require("@actions/glob");
+const fs = require("fs");
+const path = require("path");
 
+class InputMissingError extends Error {
+  constructor(inputName) {
+    super(`❌ Missing required input: ${inputName}`);
+  }
+}
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const pth = core.getInput("path") || "*.md";
+    const libName = core.getInput("libName");
+    const indexLink = core.getInput("indexLink");
+    const homeLink = core.getInput("homeLink");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    if (!pth) {
+      throw new InputMissingError("path");
+    }
+    if (!libName) {
+      throw new InputMissingError("libName");
+    }
+    if (!indexLink) {
+      throw new InputMissingError("indexLink");
+    }
+    if (!homeLink) {
+      throw new InputMissingError("homeLink");
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    const glob = await globber.create("*.md\n!index.md");
+    const files = await glob.glob();
+    const convert = (str) => {
+      // replace index.md
+      if (str === "./index.md") {
+        str = homeLink;
+      }
+
+      // replace all libNames
+      str = str.replaceAll(libName, indexLink);
+
+      // strip md extensions
+      if (str.endsWith(".md")) {
+        str = str.substring(0, str.length - 3);
+      }
+
+      // replace all dots
+      str = str.replaceAll(".", "-");
+
+      // fix ./ paths
+      if (str.startsWith("-/")) {
+        str = str.replace("-/", "./");
+      }
+
+      return str;
+    };
+
+    console.log(files);
+
+    files.forEach((file) => {
+      let contents = fs.readFileSync(file, "utf-8");
+      const replacements = [];
+
+      const mdLinkRe = /\[(?<title>.+?)\]\((?<link>.+?)\)/g;
+      let match = null;
+      while ((match = mdLinkRe.exec(contents)) !== null) {
+        const len = match[0].length;
+        const title = match.groups["title"];
+        const link = convert(match.groups["link"]);
+
+        replacements.push({
+          start: match.index,
+          end: match.index + len,
+          title,
+          link,
+        });
+      }
+
+      for (let i = 0; i < replacements.length; i++) {
+        const re = replacements[i];
+        const beforeSize = contents.length;
+        contents = `${contents.substring(0, re.start)}[${re.title}](${
+          re.link
+        })${contents.substring(re.end)}`;
+        const afterSize = contents.length;
+
+        for (let j = 0; j < replacements.length; j++) {
+          replacements[j].start += afterSize - beforeSize;
+          replacements[j].end += afterSize - beforeSize;
+        }
+      }
+
+      const outFile =
+        path.resolve(path.dirname(file), convert(path.basename(file))) +
+        path.extname(file);
+
+      console.log(`📃 Writing to file '${outFile}'.`);
+
+      fs.writeFileSync(outFile, contents, { encoding: "utf-8" });
+
+      if (file !== outFile) {
+        fs.rmSync(file);
+      }
+
+      console.log("✅ Complete");
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
